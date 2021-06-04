277c574ed08dffb8432d828fe0f36bc5d36f90ad
Gamaliel <gabanto@sigcomt.com>
2021-06-04 17:34:02 -0500

    Subiendo proyecto editor de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { ToolbarService, DocumentEditorContainerComponent } from '@syncfusion/ej2-angular-documenteditor';
+import { TitleBar } from '../../title-bar';
+import { defaultDocument, WEB_API_ACTION } from '../../data';
+import { isNullOrUndefined } from '@syncfusion/ej2-base';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ToolbarService]
 })
 export class AppComponent {
-  title = 'document-editor';
+  public hostUrl: string = 'https://ej2services.syncfusion.com/production/web-services/';
+  @ViewChild('documenteditor_default')
+  public container: DocumentEditorContainerComponent;
+  public culture: string = 'en-US';
+  titleBar: TitleBar;
+
+  onCreate(): void {
+      let titleBarElement: HTMLElement = document.getElementById('default_title_bar');
+      this.titleBar = new TitleBar(titleBarElement, this.container.documentEditor, true);
+      this.container.serviceUrl = this.hostUrl + WEB_API_ACTION;
+      this.container.documentEditor.open(JSON.stringify(defaultDocument));
+      this.container.documentEditor.documentName = 'Getting Started';
+      this.titleBar.updateDocumentTitle();
+  }
+
+  onDocumentChange(): void {
+      if (!isNullOrUndefined(this.titleBar)) {
+          this.titleBar.updateDocumentTitle();
+      }
+      this.container.documentEditor.focusIn();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {DocumentEditorContainerModule, DocumentEditorModule } from '@syncfusion/ej2-angular-documenteditor';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DocumentEditorModule,
+    DocumentEditorContainerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
